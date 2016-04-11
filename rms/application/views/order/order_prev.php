59409608989bf4725753bb8bd68c83507ac2eaec
fanzila <pierre@doleans.net>
2016-04-11 20:26:05 +0200

    bug fix

--- rms/application/views/order/order_prev.php
+++ rms/application/views/order/order_prev.php
@@ -10,7 +10,7 @@
 				<? foreach ($order as $rec) { ?>
 					<li data-role="list-divider"><?=$rec['date']?> | <?=$rec['supplier_name']?> |  <?=$rec['first_name']?> <?=$rec['last_name']?> | ID: <?=$rec['idorder']?> | Status commande: <?=$rec['status']?> <?if($rec['status'] == 'sent') { ?> | Status confirmation : <?=$rec['confirm']?> <? } ?>
 
-							<li data-inset="true" data-split-theme="a"> <a rel="external" data-ajax="false" href="/order/downloadOrder/<?=$rec['idorder']?>_<?=$rec['supplier_name']?>">View BDC</a></li>
+							<?if($rec['status'] != 'draft') { ?><li data-inset="true" data-split-theme="a"> <a rel="external" data-ajax="false" href="/order/downloadOrder/<?=$rec['idorder']?>_<?=$rec['supplier_name']?>">View BDC</a></li><? } ?>
 							<li data-inset="true" data-split-theme="a"> <a rel="external" data-ajax="false" href="/order/viewProducts/0/<?=$rec['idorder']?>">Open order</a></li>
 					<? } ?>
 				</ul>
