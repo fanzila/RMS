5824eb0427a2b626ee8ad1ce9c55646383bb6b58
fanzila <pierre@doleans.net>
2016-06-26 22:51:30 +0200

    clean

--- rms/application/controllers/order.php
+++ rms/application/controllers/order.php
@@ -547,41 +547,4 @@ private function clean_number($num) {
 	return $t2;
 }
 }
-
-/**
-public function stock_alert()
-{
-
-if($this->input->is_cli_request()) {
-
-if($id == 3) $name ="opening";
-if($id == 4) $name ="closing";
-
-$msg = "WARINING! No $name checklist have been created!";
-
-$sql 	= "SELECT DATE(`date`) FROM ckl_checklist_records WHERE DATE(`date`) = DATE(NOW()) AND id_checklist = $id";
-$query	= $this->db->query($sql);
-$res 	= $query->result();
-
-if(empty($res)) {	
-
-$email['to']		= 'checklist@hankrestaurant.com';
-$email['subject'] 	= $msg;
-$email['msg'] 		= $msg;
-$this->mmail->sendEmail($email);
-
-$sms = array();
-$sms['to']			= "email2sms@ovh.net";
-$sms['subject'] 	= "sms-dp131762-1:hanksms:gxistf23:HANK:+33647384930,+33650925448:::1";
-$sms['msg'] 		= $email['subject'];
-#$this->mmail->sendEmail($sms);
-
-}
-return;
-} else { 
-echo "Access refused.";
-return; 
-}
-
-}
-**/
+?>
