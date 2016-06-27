87a5e6a98141b1fc4129946d1dd85adb99f48a2c
fanzila <pierre@doleans.net>
2016-06-27 22:41:24 +0200

    update session data

--- rms/application/config/ion_auth.php
+++ rms/application/config/ion_auth.php
@@ -84,7 +84,7 @@ $config['salt_prefix']    = '$2y$';
  | The controller should check this function and act
  | appropriately. If this variable set to 0, there is no maximum.
  */
-$config['site_title']                 = "HMW";       // Site Title, example.com
+$config['site_title']                 = "RMS";       // Site Title, example.com
 $config['admin_email']                = "contact@hankrestaurant.com"; // Admin Email, admin@example.com
 $config['default_group']              = 'user';           // Default group, use name
 $config['admin_group']                = 'admin';             // Default administrators group, use name
@@ -94,7 +94,7 @@ $config['max_password_length']        = 20;                  // Maximum Allowed 
 $config['email_activation']           = FALSE;               // Email Activation for registration
 $config['manual_activation']          = FALSE;               // Manual Activation for registration
 $config['remember_users']             = TRUE;                // Allow users to be remembered and enable auto-login
-$config['user_expire']                = 0;               // How long to remember the user (seconds). Set to zero for no expiration
+$config['user_expire']                = 1500;               // How long to remember the user (seconds). Set to zero for no expiration
 $config['user_extend_on_login']       = TRUE;               // Extend the users cookies every time they auto-login
 $config['track_login_attempts']       = FALSE;               // Track the number of failed login attempts for each user or ip.
 $config['track_login_ip_address']     = TRUE;                // Track login attempts by IP Address, if FALSE will track based on identity. (Default: TRUE)
