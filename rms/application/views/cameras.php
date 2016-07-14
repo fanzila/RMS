c4bbfa44906c5b3e1fe03f356c63060687b8fd1b
fanzila <pierre@doleans.net>
2016-07-14 12:30:28 +0200

    camera bug fix

--- rms/application/views/cameras.php
+++ rms/application/views/cameras.php
@@ -8,27 +8,105 @@
 	<meta http-equiv="pragma" content="no-cache">
 </head>
 <body>
+<?if(isset($cam1)) { ?>
+	<SCRIPT LANGUAGE="JavaScript">
+	// Set the BaseURL to the URL of your camera
+	//Example: var BaseURL = "http://172.21.1.122/";
 
-			
-	<style type="text/css">
-	#img1 {
-	    width: auto;
-	    height: auto;
-		border: 1px solid #b1b1b1;
-		float: right;
+	// The two following lines need to be changed if an an AXIS 241S(A)/241Q(A)/240Q/243SA is used:
+	var Camera = ""; // If you use an AXIS 240Q/241S(A)/241Q(A)/243SA, comment away this line by inserting "//"
+	// This is the path to the image generating file inside the camera itself
+	var File = "<?=$cam1?>";
+	// No changes required below this point
+	if (Camera != "") {File += "&camera=" + Camera;}
+	var output = "";
+	if ((navigator.appName == "Microsoft Internet Explorer") &&
+	(navigator.platform != "MacPPC") && (navigator.platform != "Mac68k"))
+	{
+	// If Internet Explorer under Windows then use ActiveX
+	output = '<OBJECT ID="Player" width='
+	output += DisplayWidth;
+	output += ' height=';
+	output += DisplayHeight;
+	output += ' CLASSID="CLSID:DE625294-70E6-45ED-B895-CFFA13AEB044" ';
+	output += 'CODEBASE="';
+	output += 'activex/AMC.cab">';
+	output += '<PARAM NAME="MediaURL" VALUE="';
+	output += File + '">';
+	output += '<param name="MediaType" value="mjpeg-unicast">';
+	output += '<param name="ShowStatusBar" value="0">';
+	output += '<param name="ShowToolbar" value="0">';
+	output += '<param name="AutoStart" value="1">';
+	output += '<param name="StretchToFit" value="1">';
+	output += '<BR><B>Axis Media Control</B><BR>';
+	output += 'The AXIS Media Control, which enables you ';
+	output += 'to view live image streams in Microsoft Internet';
+	output += ' Explorer, could not be registered on your computer.';
+	output += '<BR></OBJECT>';
+	} else {
+	// If not IE for Windows use the browser itself to display
+	theDate = new Date();
+	output = '<IMG SRC="';
+	output += File;
+	output += '&dummy=' + theDate.getTime().toString(10);
+	output += '" HEIGHT="';
+	output += '" WIDTH="';
+	output += '" ALT="Camera Image">';
 	}
+	document.write(output);
+	</SCRIPT>
+<? } ?>
 
-	#img2 {
-	    width: auto;
-	    height: auto;
-		border: 1px solid #b1b1b1;
-		float: left;
+<?if(isset($cam2)) { ?>
+	<SCRIPT LANGUAGE="JavaScript">
+	// Set the BaseURL to the URL of your camera
+	//Example: var BaseURL = "http://172.21.1.122/";
+
+	// The two following lines need to be changed if an an AXIS 241S(A)/241Q(A)/240Q/243SA is used:
+	var Camera = ""; // If you use an AXIS 240Q/241S(A)/241Q(A)/243SA, comment away this line by inserting "//"
+	// This is the path to the image generating file inside the camera itself
+	var File = "<?=$cam2?>";
+	// No changes required below this point
+	if (Camera != "") {File += "&camera=" + Camera;}
+	var output = "";
+	if ((navigator.appName == "Microsoft Internet Explorer") &&
+	(navigator.platform != "MacPPC") && (navigator.platform != "Mac68k"))
+	{
+	// If Internet Explorer under Windows then use ActiveX
+	output = '<OBJECT ID="Player" width='
+	output += DisplayWidth;
+	output += ' height=';
+	output += DisplayHeight;
+	output += ' CLASSID="CLSID:DE625294-70E6-45ED-B895-CFFA13AEB044" ';
+	output += 'CODEBASE="';
+	output += 'activex/AMC.cab">';
+	output += '<PARAM NAME="MediaURL" VALUE="';
+	output += File + '">';
+	output += '<param name="MediaType" value="mjpeg-unicast">';
+	output += '<param name="ShowStatusBar" value="0">';
+	output += '<param name="ShowToolbar" value="0">';
+	output += '<param name="AutoStart" value="1">';
+	output += '<param name="StretchToFit" value="1">';
+	output += '<BR><B>Axis Media Control</B><BR>';
+	output += 'The AXIS Media Control, which enables you ';
+	output += 'to view live image streams in Microsoft Internet';
+	output += ' Explorer, could not be registered on your computer.';
+	output += '<BR></OBJECT>';
+	} else {
+	// If not IE for Windows use the browser itself to display
+	theDate = new Date();
+	output = '<IMG SRC="';
+	output += File;
+	output += '&dummy=' + theDate.getTime().toString(10);
+	output += '" HEIGHT="';
+	output += '" WIDTH="';
+	output += '" ALT="Camera Image">';
 	}
-	</style>
+	document.write(output);
+	</SCRIPT>
+<? } ?>
 
-<?if(isset($cam1)) { ?><div id="img"><img id="img1" src="<?=$cam1?>"></div><? } ?>
-<?if(isset($cam2)) { ?><div id="img"><img id="img2" src="<?=$cam2?>"></div><? } ?>
-<?if(isset($cam3)) { ?><div id="img"><img id="img1" src="<?=$cam3?>"></div><? } ?>
-<?if(isset($cam4)) { ?><div id="img"><img id="img2" src="<?=$cam4?>"></div><? } ?>
+<?if(isset($cam3)) { ?><img src="<?=$cam3?>"><? } ?>
+<?if(isset($cam4)) { ?><img src="<?=$cam4?>"><? } ?>
 </body>
 </html>
