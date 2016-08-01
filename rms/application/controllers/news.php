5e90d86c17ca1499248c95058dc74d01df0c89fb
fanzila <pierre@doleans.net>
2016-08-01 10:22:03 +0200

    resolve conflicts

--- rms/application/controllers/news.php
+++ rms/application/controllers/news.php
@@ -1,10 +1,8 @@
 <?php
 class News extends CI_Controller {
-
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->database();
 		$this->load->model('news_model');
 		$this->load->helper('url_helper');
 		$this->load->library("pagination");
@@ -13,39 +11,29 @@ class News extends CI_Controller {
 		$this->load->library("hmw");
 		$this->load->helper('html');
 	}
-
 	public function view($slug = NULL)
 	{
 		$data['news_item'] = $this->news_model->get_news($slug);
-
 		if (empty($data['news_item']))
 		{
 			show_404();
 		}
-
-		$header['title'] = $data['news_item']['title'];
-
-		$this->load->view('jq_header_pre', $header);
-		$this->load->view('news/jq_header_spe');
-		$this->load->view('jq_header_post');
+		$data['title'] = $data['news_item']['title'];
+		$this->load->view('jq_header', $data);
 		$this->load->view('news/view', $data);
 		$this->load->view('jq_footer');
 	}
-
-	public function index($login=null)
+	public function index()
 	{
-		if (!$this->ion_auth->logged_in())
-		{
-			redirect('auth/login');
-		}
-		$this->hmw->changeBu();// GENERIC changement de Bu
+		$data = array();
 		
 		$this->hmw->keyLogin();
 		
 		$user					= $this->ion_auth->user()->row();
 		$user_groups 			= $this->ion_auth->get_users_groups()->result();
-		$bus_list = $this->hmw->getBus(null, $user->id);
-				
+		$data['username']		= $user->username;
+		$data['user_groups']	= $user_groups[0];
+		$data['title'] 			= 'News';
 		$config = array();
 		$config["base_url"] = base_url() . "news/index";
 		$config["total_rows"] = $this->news_model->record_count();
@@ -53,74 +41,42 @@ class News extends CI_Controller {
 		$config["uri_segment"] = 3;
 		$choice = $config["total_rows"] / $config["per_page"];
 		$config["num_links"] = round($choice);
-
 		$this->pagination->initialize($config);
-
 		$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+		$data["keylogin"] = $this->session->userdata('keylogin');
+		$data["results"] = $this->news_model->get_list($config["per_page"], $page);
+		$data["links"] = $this->pagination->create_links();
+		$data['bu_name'] =  $this->session->all_userdata()['bu_name'];
 		
-		$data = array(
-			'username'	=> $user->username,
-			'user_groups'	=> $user_groups[0],
-			'title'		=> 'News',
-			'keylogin'	=> $this->session->userdata('keylogin'),
-			'results'	=> $this->news_model->get_list($config["per_page"], $page),
-			'links'		=> $this->pagination->create_links(),
-			'login'		=> $login,
-			'bu_name'	=> $this->session->all_userdata()['bu_name']
-			);
-
-		$headers = $this->hmw->headerVars(1, "/news/index/", "News");
-		$this->load->view('jq_header_pre', $headers['header_pre']);
-		$this->load->view('jq_header_post', $headers['header_post']);
-		$this->load->view('news/index',$data);
+		$this->load->view('jq_header', $data);
+		$this->load->view('news/index', $data);
 		$this->load->view('jq_footer');
 	}
-
 	public function create()
 	{
-
-
 		if (!$this->ion_auth->logged_in())
 		{
 			redirect('auth/login');
 		}
-
 		$group_info = $this->ion_auth_model->get_users_groups()->result();
 		if ($group_info[0]->level < 1)
 		{
 			$this->session->set_flashdata('message', 'You must be a gangsta to view this page');
-			redirect('/news/');
+			redirect('/admin/');
 		}
-
 		$user = $this->ion_auth->user()->row();
-
 		$bus_list = $this->hmw->getBus(null, $user->id);
 		
 		$this->load->helper('form');
-<<<<<<< HEAD
-
 		$data['bu_name'] =  $this->session->all_userdata()['bu_name'];
-
 		$data['title']	= 'Create a news item';
 		$data['from']	= $user->username;
 		$data['bus_list']	= $bus_list;
 		$data['bu_id']	= $this->session->all_userdata()['bu_id'];
 		$data['error']	= '';
-=======
->>>>>>> master
 		
-		$headers = $this->hmw->headerVars(0, "/news/index/", "Create News");
-
-<<<<<<< HEAD
 		if (!$this->input->post('title')){
 			$this->load->view('jq_header', $data);
-=======
-		if (!$this->input->post('title'))
-		{
-			$this->load->view('jq_header_pre', $headers['header_post']);
-			$this->load->view('news/jq_header_spe');
-			$this->load->view('jq_header_post', $headers['header_post']);
->>>>>>> master
 			$this->load->view('news/create');
 			$this->load->view('jq_footer');
 		}else{
@@ -146,16 +102,13 @@ class News extends CI_Controller {
 			if($error == 0){
 				$news_id = $this->news_model->set_news($user->id);
 			}
-
 			if ($checkUpload){
 				$this->db->set('picture', $picName)->where('id', $news_id);
 				$this->db->update('news');
 			}
-
 			if($error == 0){
 				$server_name = $this->hmw->getParam('server_name'); 
 				
-<<<<<<< HEAD
 				$this->load->library('mmail');
 				$bus = $this->input->post('bus');
 				
@@ -180,18 +133,15 @@ class News extends CI_Controller {
 						);
 				
 					$this->db->insert('news_confirm', $confi);
-
 					$email['from']		= 'news@hankrestaurant.com';
 					$email['from_name']	= 'HANK NEWS';
 					$email['to']		= $row->email;
 					$email['replyto'] 	= "news@hankrestaurant.com";
 					$email['subject']	= 'Hank News! '.$this->input->post('title');
 					$email['mailtype']	= 'html';
-
 					$msg = $this->input->post('text');
 					$msg .= "\r\n\r\n->>>>Merci de confirmer la lecture de ce message en cliquant ici : $link";
 					$msg .= "\r\n-- \r\n$user->username";
-
 					$email['msg'] = $msg;
 					
 					$this->mmail->sendEmail($email);
@@ -205,23 +155,10 @@ class News extends CI_Controller {
 				$this->load->view('news/success');
 				$this->load->view('jq_footer');
 			}		
-=======
-				$this->mmail->sendEmail($email);
-			}
-
-			$this->load->view('jq_header_pre', $headers['header_pre']);
-			$this->load->view('news/jq_header_spe');
-			$this->load->view('jq_header_post', $headers['header_post']);
-			$this->load->view('news/success');
-			$this->load->view('jq_footer');
->>>>>>> master
 		}
 	}
-
 	public function confirm($key = null) {
-
 		$this->load->library('mmail');
-
 		$this->db->join('users', 'news_confirm.id_user = users.id');
 		$this->db->limit(1) or die($this->mysqli->error);				
 		$res = $this->db->get_where('news_confirm', array('key' => $key));
@@ -242,9 +179,7 @@ class News extends CI_Controller {
 		} else {
 			$data = array('status' => 'NOK');	
 		}
-
 		$this->load->view('news/confirm',$data);
 	}
-
 }
 ?>
