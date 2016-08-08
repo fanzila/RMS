163e70f46962e787b6230d4b4cd8679a587a2779
clementp92 <clement.pabst@reseau.eseo.fr>
2016-08-08 10:01:43 +0200

    Report
    enhancement of the body size in compose.

--- rms/application/views/pm/jq_header_spe.php
+++ rms/application/views/pm/jq_header_spe.php
@@ -5,6 +5,7 @@
 			selector: "#<?=TF_PM_BODY?>",
 			plugins: "textcolor hr fullscreen",
 			toolbar: "forecolor backcolor fullscreen",
+			min_height: 275,
 			removed_menuitems: 'newdocument'
 		});
     </script>
