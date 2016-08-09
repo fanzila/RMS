84c28b658363ecca185d01d07a43ba208a1aeef7
clementp92 <clement.pabst@reseau.eseo.fr>
2016-08-09 16:12:21 +0200

    Skills
    - Parts of the managing part
    - Man can sees it's own advancement
    - tiny fix on report.

--- rms/application/controllers/skills.php
+++ rms/application/controllers/skills.php
@@ -29,11 +29,24 @@ class Skills extends CI_Controller {
 		$this->load->library('form_validation');
 	}
 
-	public function index($task_id = null)
+	public function index($id = null)
 	{
 		$this->hmw->changeBu();// GENERIC changement de Bu
-
-		$this->hmw->keyLogin();//->est-ce que cela doit être accessible via keylogin? non tel que conçu je suppose
+		if($id!= null && $id!=$this->ion_auth->get_user_id()){
+			if (!$this->ion_auth->logged_in() || (!$this->ion_auth->is_admin() && !($this->ion_auth->user()->row()->id == $id))){
+				redirect('skills', 'refresh');
+			}else{
+				$this->db->select('users.username, users.last_name, users.first_name, users.email, users.id');
+				$this->db->distinct('users.username');
+				$this->db->join('users_bus', 'users.id = users_bus.user_id', 'left');
+				$this->db->where('users.id', $id);
+				$this->db->order_by('users.username', 'asc');
+				$query = $this->db->get("users");
+				$user = $query->result();
+			}
+		}else{
+			$id = $this->ion_auth->get_user_id();
+		}
 		$id_bu =  $this->session->all_userdata()['bu_id'];
 
 		/* SPECIFIC Recuperation depuis la base de donnees des informations discounts */
@@ -45,21 +58,10 @@ class Skills extends CI_Controller {
 			->join('skills', 'skills.id = I.id_skills')
 			->join('skills_category as cat', 'I.id_cat = cat.id')
 			->join('skills_sub_category as subcat', 'I.id_sub_cat = subcat.id')
-			->where('R.id_user', $this->ion_auth->get_user_id())
+			->where('R.id_user', $id)
 			->order_by('I.name desc');
 		$res 	= $this->db->get() or die($this->mysqli->error);
 		$skills_items = $res->result();
-/*foreach ($skills_items as $item) {
-	echo $item->id."\n";
-	echo $item->id_user."\n";
-	echo $item->checked."\n";
-	echo $item->comment."\n";
-	echo $item->i_name."\n";
-	echo $item->s_name."\n";
-	echo $item->c_name."\n";
-	echo $item->sub_name."\n";
-}*/
-
 
 		$this->db->select('id, name')
 			->from('skills')
@@ -90,20 +92,119 @@ class Skills extends CI_Controller {
 		$data['bu_name'] =  $this->session->all_userdata()['bu_name'];
 		$data['username'] = $this->session->all_userdata()['identity'];
 
-		$headers = $this->hmw->headerVars(1, "/skills/", "My skills");
-		$this->load->view('jq_header_pre', $headers['header_pre']);
-		$this->load->view('jq_header_post', $headers['header_post']);
-		$this->load->view('skills/index',$data);
-		$this->load->view('jq_footer');
+		if($id != $this->ion_auth->get_user_id()){
+			$headers = $this->hmw->headerVars(0, "/skills/admin", "Skills of ".$user[0]->first_name." ".$user[0]->last_name);
+			$this->load->view('jq_header_pre', $headers['header_pre']);
+			$this->load->view('jq_header_post', $headers['header_post']);
+			$this->load->view('skills/index',$data);
+			$this->load->view('jq_footer');
+		}else{
+			$headers = $this->hmw->headerVars(1, "/skills/", "My Skills");
+			$this->load->view('jq_header_pre', $headers['header_pre']);
+			$this->load->view('jq_header_post', $headers['header_post']);
+			$this->load->view('skills/index',$data);
+			$this->load->view('jq_footer');
+		}
 	}
 	
-	public function admin)(
+	public function admin()
 	{
+		if (!$this->ion_auth->logged_in() || (!$this->ion_auth->is_admin())) {
+				redirect('skills', 'refresh');
+		}
+		$id_bu =  $this->session->all_userdata()['bu_id'];
+		/* SPECIFIC Recuperation depuis la base de donnees des informations users */
+		$this->db->select('users.username, users.last_name, users.first_name, users.email, users.id');
+		$this->db->distinct('users.username');
+		$this->db->join('users_bus', 'users.id = users_bus.user_id', 'left');
+		$this->db->where('users.active', 1);
+		$this->db->where('users_bus.bu_id', $id_bu);
+		$this->db->order_by('users.username', 'asc');
+		$query = $this->db->get("users");
+		$users = $query->result();
+
+/* SPECIFIC used for consulting a staff's qualifications*/
+		date_default_timezone_set('Europe/Paris');
+		$this->db->select('R.id, R.id_user, RI.checked, RI.comment, I.name as i_name, skills.name as s_name, cat.name as c_name, subcat.name as sub_name')
+			->from('skills_record as R')
+			->join('skills_record_item as RI', 'R.id = RI.id_skills_record')
+			->join('skills_item as I', 'I.id = RI.id_skills_item')
+			->join('skills', 'skills.id = I.id_skills')
+			->join('skills_category as cat', 'I.id_cat = cat.id')
+			->join('skills_sub_category as subcat', 'I.id_sub_cat = subcat.id')
+			->order_by('I.name desc');
+		$res 	= $this->db->get() or die($this->mysqli->error);
+		$skills_staff = $res->result();
+
+		/* SPECIFIC used for creating new sponsorship link */
+		date_default_timezone_set('Europe/Paris');
+		$this->db->select('R.id, R.id_user, RI.checked, RI.comment, I.name as i_name, skills.name as s_name, cat.name as c_name, subcat.name as sub_name')
+			->from('skills_record as R')
+			->join('skills_record_item as RI', 'R.id = RI.id_skills_record')
+			->join('skills_item as I', 'I.id = RI.id_skills_item')
+			->join('skills', 'skills.id = I.id_skills')
+			->join('skills_category as cat', 'I.id_cat = cat.id')
+			->join('skills_sub_category as subcat', 'I.id_sub_cat = subcat.id')
+			->order_by('I.name desc');
+		$res 	= $this->db->get() or die($this->mysqli->error);
+		$skills_items = $res->result();
+
+		$this->db->select('id, name')
+			->from('skills')
+			->order_by('name desc');
+		$res 	= $this->db->get() or die($this->mysqli->error);
+		$skills = $res->result();
+
+		$this->db->select('id, name')
+			->from('skills_category')
+			->order_by('name desc');
+		$res 	= $this->db->get() or die($this->mysqli->error);
+		$skills_categories = $res->result();
+
+		$this->db->select('id, name')
+			->from('skills_sub_category')
+			->order_by('name desc');
+		$res 	= $this->db->get() or die($this->mysqli->error);
+		$skills_sub_categories = $res->result();
+
+
+
+		$data = array(
+			'skills'	=> $skills,
+			'skills_categories'	=> $skills_categories,
+			'skills_sub_categories'	=> $skills_sub_categories,
+			'skills_items'	=> $skills_items,
+			'skills_staff' => $skills_staff
+			);
+
+		$data['users'] = $users;
 		$headers = $this->hmw->headerVars(1, "/skills/admin", "Skills Management");
 		$this->load->view('jq_header_pre', $headers['header_pre']);
 		$this->load->view('jq_header_post', $headers['header_post']);
-		$this->load->view('skills/admin');
+		$this->load->view('skills/admin', $data);
 		$this->load->view('jq_footer');
+	}
+
+	public function save()
+	{		
+		$data = $this->input->post();
+		
+		$reponse = 'ok';
+		$this->db->set('id_sponsor', $data['sponsor']);
+		$this->db->set('id_user', $data['user']);
+
+		$this->db->trans_start();
+			if(!$this->db->insert('skills_record')) {
+				$response = "Can't place the insert sql request, error message: ".$this->db->_error_message();
+			}
+			$data['id'] = $this->db->insert_id();
+			
+			/*if(!$this->db->insert('discount_log')) {
+				$response = "Can't place the insert sql request, error message: ".$this->db->_error_message();
+			}*/
+		$this->db->trans_complete();
+
+		echo json_encode(['reponse' => $reponse]);
 	}
 
 /*	public function log($admin = null)
---
+++ rms/application/views/skills/admin.php
@@ -0,0 +1,155 @@
+		</div>
+		<div data-role="content">
+			<div data-role="collapsible-set" data-inset="true">
+				<div data-role="collapsible">
+					<h1>Create a sponsoring link</h1>
+					<?$attributes = array('id' => "sponsorship", 'name' => "sponsorship");
+					echo form_open("skills/save", $attributes);?>
+						<table width="100%" style="border: 1px solid #dedcd7; margin-top:10px" cellpadding="8">
+							<tr>
+								<td colspan="2" style="background-color: #fbf19e;">Sponsorship to create :
+								</td>
+							</tr>
+							<tr>
+								<td width="6%"><label for="sponsor" id="label">Sponsor :</label></td>
+								<td width="94%">
+									<select style="background-color:#a1ff7c" name="sponsor" id="sponsor" data-inline="true" data-theme="a" required>
+										<option value="">Sponsor</option>
+										<?foreach ($users as $user) {?>
+											<option value="<?=$user->id?>" <? if(isset($form['user']) AND $form['user']==$user->id) { ?> selected <? } ?>><?=$user->first_name?> <?=$user->last_name?>
+											</option>
+										<? } ?>
+									?></select>
+								</td>
+							</tr>
+							<tr>
+								<td><label for="user" id="label">Trainee:</label></td>
+								<td>
+									<select style="background-color:#a1ff7c" name="user" id="user" data-inline="true" data-theme="a" required>
+										<option value="">Trainee</option>
+										<?foreach ($users as $user) {?>
+											<option value="<?=$user->id?>" <? if(isset($form['user']) AND $form['user']==$user->id) { ?> selected <? } ?>><?=$user->first_name?> <?=$user->last_name?>
+											</option>
+										<? } ?>
+									?></select>
+								</td>
+							</tr>
+							<tr><td>. . . . . .</td><td><p>Ensuite se débrouiller pour avoir la sélection des skills du parrain de disponible pour sélectionner ce qu'il doit enseigner.</p></td></tr>
+						</table>
+						<?$attributes = array('id' => "sub", 'name' => "submit");
+						echo form_submit($attributes, 'Save');?>
+					</form>
+
+					<script>
+					$(document).ready(function() {
+
+						var $form = $('#sponsorship');
+
+						$('#sub').on('click', function() {
+							$form.trigger('submit');
+							return false;
+						});
+
+						$form.on('submit', function() {
+
+							var sponsor = $('#sponsor').val();
+							var user = $('#user').val();
+
+							if(sponsor == '') {
+								alert('Please fill sponsor.');
+							} else if(user == 0){
+								alert('Please indicate who has to learn.');
+							} else if(user == sponsor){
+								alert('You have to choose two different people.');
+							}else {
+								$.ajax({
+									url: $(this).attr('action'),
+									type: $(this).attr('method'),
+									data: $(this).serialize(),
+									dataType: 'json',
+									success: function(json) {
+										if(json.reponse == 'ok') {
+											alert('Saved!');
+										} else {
+											alert('WARNING! ERROR at saving : '+ json.reponse);
+										}
+									}
+								}).done(function(data) {
+										window.location = "/skills/index/admin/";
+								    }).fail(function(data) {
+								    	alert('WARNING! ERROR at saving!');
+								    });
+							}
+							return false;
+						});
+					});
+					</script>
+				</div><!--/collapsible-->
+				<div data-role="collapsible">
+					<h1>Skills map</h1>
+					<div data-role="collapsible-set" data-inset="true">
+							<?foreach ($skills as $skill) {?><!--Affichage des skills générales-->
+								<?$check=0;foreach($skills_items as $skills_item){
+									if($skills_item->s_name == $skill->name) $check+=1;
+									if($check == 1) break;
+								}?>
+								<?if($check==1){?>
+									<div data-role="collapsible" data-inset="true">
+										<h4><?=$skill->name?></h4>
+										<div data-inset="true">
+											<?foreach ($skills_categories as $category) {?><!--Affichage des Catégories-->
+												<?$check=0;foreach($skills_items as $skills_item){
+													if($skills_item->c_name == $category->name) $check+=1;
+													if($check == 1) break;
+												}?>
+												<?if($check==1){?>
+													<div data-role="collapsible" data-inset="true">
+														<h4><?=$category->name?></h4>
+														<div data-inset="true">
+															<?foreach($users as $user){
+																$validated=0;
+																foreach ($skills_items as $skills_item) {
+																	if($skills_item->c_name == $category->name && $skills_item->id_user == $user->id){
+																		if($skills_item->checked == true){
+																			$validated=1;
+																		}else{
+																			$validated=0;
+																			break;
+																		}
+																	}
+																}
+																if($validated == 1){
+																	?><div data-inset="true"><?=$user->username?></div><?
+																}
+															}?>
+														</div><!-- /insert for sub categories -->
+													</div><!-- /collapsible -->
+												<?}?>
+											<?}?>
+										</div><!-- /inset for categories -->
+									</div><!-- /collapsible -->
+								<?}?>
+							<?}?>
+						</div><!-- /skills filter -->
+				</div>
+				<div data-role="collapsible">
+					<h1>Skills by person</h1>
+					<select style="background-color:#a1ff7c" name="sponsor" id="sponsor" data-inline="true" data-theme="a" onchange="getContent(this)">
+						<option value="">Sponsor</option>
+						<?foreach ($users as $user) {?>
+							<option value="<?=$user->id?>" <? if(isset($form['user']) AND $form['user']==$user->id) { ?> selected <? } ?>><?=$user->first_name?> <?=$user->last_name?>
+							</option>
+						<? } ?>
+					?></select>
+					<div data-role="collapsible-set" data-inset="true">
+					</div>
+				</div>
+				<script type="text/javascript">
+					function getContent(select) {
+						var test = select.value;
+						if(test!="")window.location = "/skills/index/"+test;
+					}
+				</script>
+			</div><!--/collapsible set-->
+		</div><!-- /content -->
+	</div><!-- /page -->
