01408aa5e79610e3f7e59731872972e91db30ee6
fanzila <pierre@doleans.net>
2016-08-18 14:45:13 +0200

    typo fix

--- rms/application/libraries/Cashier.php
+++ rms/application/libraries/Cashier.php
@@ -226,7 +226,7 @@ class Cashier {
 		$CI->load->database();
 		$CI->load->library("hmw");
 		$file	= $this->getPosDbDir($param['id_bu']);
-		if(!file_exists($file)) exit('No db found for BU ID'.$param['id_bu'].' in '.$file);
+		if(!file_exists($file)) exit('No POS db found for BU ID '.$param['id_bu'].' in '.$file);
 		$db	= new SQLite3($file);
 		
 		$getBuInfo = $CI->hmw->getBuInfo($param['id_bu']);
