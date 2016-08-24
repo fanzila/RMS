0d4c2fdd4c8924484c923f6c5a57a210a4a31d55
clementp92 <clement.pabst@reseau.eseo.fr>
2016-08-24 11:39:05 +0200

    order log
    filter by supplier, date, user, command id, status

--- rms/application/views/order/order_prev.php
+++ rms/application/views/order/order_prev.php
@@ -1,11 +1,17 @@
 		</div>
 		<div data-role="content" data-theme="a">
-			<ul data-role="listview" data-inset="true"  data-filter="false">
+			<ul data-role="listview" data-inset="true"  data-filter="true">
 				<? foreach ($order as $rec) { ?>
 					<li data-role="list-divider"><?=$rec['date']?> | <?=$rec['supplier_name']?> |  <?=$rec['first_name']?> <?=$rec['last_name']?> | ID: <?=$rec['idorder']?> | Status commande: <?=$rec['status']?> <?if($rec['status'] == 'sent') { ?> | Status confirmation : <?=$rec['confirm']?> <? } ?>
 
-							<?if($rec['status'] != 'draft') { ?><li data-inset="true" data-split-theme="a"> <a rel="external" data-ajax="false" href="/order/downloadOrder/<?=$rec['idorder']?>_<?=$rec['supplier_name']?>">View BDC</a></li><? } ?>
-							<li data-inset="true" data-split-theme="a"> <a rel="external" data-ajax="false" href="/order/viewProducts/0/<?=$rec['idorder']?>">Open order</a></li>
+							<?if($rec['status'] != 'draft') { ?>
+								<li data-inset="true" data-split-theme="a" data-filtertext="<?=$rec['date']?> <?=$rec['supplier_name']?> <?=$rec['first_name']?> <?=$rec['last_name']?> <?=$rec['idorder']?> <?=$rec['status']?>">
+									<a rel="external" data-ajax="false" href="/order/downloadOrder/<?=$rec['idorder']?>_<?=$rec['supplier_name']?>">View BDC</a>
+								</li>
+							<?}?>
+							<li data-inset="true" data-split-theme="a" data-filtertext="<?=$rec['date']?> <?=$rec['supplier_name']?>	<?=$rec['first_name']?> <?=$rec['last_name']?> <?=$rec['idorder']?> <?=$rec['status']?>"> <a rel="external" data-ajax="false" href="/order/viewProducts/0/<?=$rec['idorder']?>">
+								Open order</a>
+							</li>
 					<? } ?>
 				</ul>
 			</div>
