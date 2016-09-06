8a7608828b1e4ac02e15207ec0fd951ad905ed9e
fanzila <pierre@doleans.net>
2016-09-06 08:53:52 +0200

    view all cam #2

--- rms/application/views/camera/cameras.php
+++ rms/application/views/camera/cameras.php
@@ -20,6 +20,18 @@
 <?if(isset($cam4)) { ?>
 	<iframe width="640" marginheight="0" marginwidth="0" height="360" scrolling="no" frameborder="0" src="/cameras/frame/4"></iframe>			
 <? } ?>
+<?if(isset($cam5)) { ?>
+	<iframe width="640" marginheight="0" marginwidth="0" height="360" scrolling="no" frameborder="0" src="/cameras/frame/4"></iframe>			
+<? } ?>
+<?if(isset($cam6)) { ?>
+	<iframe width="640" marginheight="0" marginwidth="0" height="360" scrolling="no" frameborder="0" src="/cameras/frame/4"></iframe>			
+<? } ?>
+<?if(isset($cam7)) { ?>
+	<iframe width="640" marginheight="0" marginwidth="0" height="360" scrolling="no" frameborder="0" src="/cameras/frame/4"></iframe>			
+<? } ?>
+<?if(isset($cam8)) { ?>
+	<iframe width="640" marginheight="0" marginwidth="0" height="360" scrolling="no" frameborder="0" src="/cameras/frame/4"></iframe>			
+<? } ?>
 <p><a href="/cameras/index/allbu/1">View all cam</a></p>
 </body>
 </html>
