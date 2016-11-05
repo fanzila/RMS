c1db183b094dadc78fde18e8e6aaacc4654de8e5
fanzila <pierre@doleans.net>
2016-11-05 09:01:32 +0100

    change order index design

--- rms/application/views/order/index.php
+++ rms/application/views/order/index.php
@@ -1,81 +1,80 @@
+</div>
+<div data-role="content" data-theme="a">
+	<div class="row">
+		<div class="col-xs">
+			<div class="box">
+				<a href="/order/previousOrders/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised">Log</a>
+			</div>
 		</div>
-		<div data-role="content" data-theme="a">
-			<?php if($user_groups->level >= 1) { ?>
+		<?php if($user_groups->level >= 2) { ?>
+			<div class="col-xs">
+				<div class="box">
+					<a href="/product_admin/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised">Products admin</a>
+				</div>
+			</div>
+			<div class="col-xs">
+				<div class="box">
+					<a href="/product_admin/mapping/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised">Products mapping</a>
+				</div>
+			</div>
+			<?php if($user_groups->level >= 2) { ?>
+				<div class="col-xs">
+					<div class="box">
+						<a href="/crud/suppliers/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised" onclick="window.open(this.href);return false;">Suppliers</a>
+					</div>
+				</div>
+				<div class="col-xs">
+					<div class="box">
+						<a href="/crud/productsAttribut/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised" onclick="window.open(this.href);return false;">Products attribut</a>
+					</div>
+				</div>
 
-				<ul data-role="listview" data-inset="true" data-filter="false">
-					<li data-role="list-divider">ORDER FREQUENCY</li>
-					<?
-				foreach ($freq as $var) {
-					if($var['name'] != 'previous') {
-						?>
-						<li><a data-ajax="false" href="/order/viewProducts/<?=$var['id']?>"><?=strtoupper($var['name'])?></a></li>
-						<? }  } ?>
-					</ul>
+				<? } ?>
+			</div>
+			<br/>
+
+			<ul id="autocomplete" data-role="listview" data-inset="true" data-filter="true" data-filter-placeholder="Find a product..." data-filter-theme="d"></ul>
+			<hr />
+			<ul data-role="listview" data-inset="true" data-filter="false">
+				<li data-role="list-divider">SUPPLIERS</li>
+				<?
+			foreach ($suppliers as $varsup) {
+				?>
+				<li><a data-ajax="false" href="/order/viewProducts/1000/0/<?=$varsup['id']?>"><?=strtoupper($varsup['name'])?> 
+					<? if(isset($varsup['last_order'])) { ?><small>  <i>Last order: <?=$varsup['last_order']?> by <?=$varsup['last_order_user']->username?></i></small><? } ?></a></li>
 					<? } ?>
-					<div class="row">
-					    <div class="col-xs">
-					        <div class="box">
-								<a href="/order/previousOrders/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised">Log</a>
-					        </div>
-					    </div>
-						<?php if($user_groups->level >= 2) { ?>
-						    <div class="col-xs">
-						        <div class="box">
-						        	<a href="/product_admin/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised">Products admin</a>
-						        </div>
-						    </div>
-						    <div class="col-xs">
-						        <div class="box">
-						        	<a href="/product_admin/mapping/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised">Products mapping</a>
-						        </div>
-						    </div>
-							<?php if($user_groups->level >= 2) { ?>
-							    <div class="col-xs">
-							        <div class="box">
-							        	<a href="/crud/suppliers/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised" onclick="window.open(this.href);return false;">Suppliers</a>
-							        </div>
-							    </div>
-							    <div class="col-xs">
-							        <div class="box">
-							        	<a href="/crud/productsAttribut/" rel="external" data-ajax="false" class="ui-btn ui-btn-raised" onclick="window.open(this.href);return false;">Products attribut</a>
-							        </div>
-							    </div>
-						
-						<? } ?>
-						</div>
-						<br/>
+				</ul>
+				<? } ?>
+				<?php if($user_groups->level >= 1) { ?>
 
-						<ul id="autocomplete" data-role="listview" data-inset="true" data-filter="true" data-filter-placeholder="Find a product..." data-filter-theme="d"></ul>
-						<hr />
-						<ul data-role="listview" data-inset="true" data-filter="true">
-							<li data-role="list-divider">SUPPLIERS</li>
-							<?
-						foreach ($suppliers as $varsup) {
-								?>
-								<li><a data-ajax="false" href="/order/viewProducts/1000/0/<?=$varsup['id']?>"><?=strtoupper($varsup['name'])?> 
-								<? if(isset($varsup['last_order'])) { ?><small>  <i>Last order: <?=$varsup['last_order']?> by <?=$varsup['last_order_user']->username?></i></small><? } ?></a></li>
-								<? } ?>
-							</ul>
-					<? } ?>
-					<br /><br />						
-				</div><!-- /content -->
+					<ul data-role="listview" data-inset="true" data-filter="false">
+						<li data-role="list-divider">ORDER FREQUENCY</li>
+						<?
+					foreach ($freq as $var) {
+						if($var['name'] != 'previous') {
+							?>
+							<li><a data-ajax="false" href="/order/viewProducts/<?=$var['id']?>"><?=strtoupper($var['name'])?></a></li>
+							<? }  } ?>
+						</ul>
+						<? } ?>						
+					</div><!-- /content -->
 
-				<div id="view"></div>
-			</div><!-- /page -->
-		    <script>
+					<div id="view"></div>
+				</div><!-- /page -->
+				<script>
 				$( document ).on( "pageinit", "#orderpage", function() {
 					$( "#autocomplete" ).on( "listviewbeforefilter", function ( e, data ) {
 						var $ul = $( this ),
-							$input = $( data.input ),
-							value = $input.val(),
-							html = "";
+						$input = $( data.input ),
+						value = $input.val(),
+						html = "";
 						$ul.html( "" );
 						if ( value && value.length > 1 ) {
 							$ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
 							$ul.listview( "refresh" );
 							$.ajax({
 								url: "/order/autoCompProducts",
-								
+
 								dataType: "jsonp",
 								crossDomain: true,
 								data: {
@@ -94,4 +93,4 @@
 						}
 					});
 				});
-		    </script>
+				</script>
