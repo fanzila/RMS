80ad23f2ce396ab6ef683c10528de778cb915a54
NaelAwayes <naelawayes@gmail.com>
2017-09-28 13:46:30 +0200

    Put RMS url in params

--- rms/assets/wifiguest/sync.php
+++ rms/assets/wifiguest/sync.php
@@ -33,7 +33,7 @@ if (isset($res) && !empty($res)) {
   
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "http://rms.hankrestaurant.com/customers/record");
+curl_setopt($ch, CURLOPT_URL, "$RMSUrl/customers/record");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, array('data' => $jdata));
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -60,7 +60,7 @@ if (isset($ret['lastID'])) {
     }
   }
 } else {
-  curl_setopt($ch, CURLOPT_URL, "http://rms.hankrestaurant.com/customers/getLastId/true");
+  curl_setopt($ch, CURLOPT_URL, "$RMSUrl/customers/getLastId/true");
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   #curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
   #curl_setopt($ch, CURLOPT_PORT,  443);
