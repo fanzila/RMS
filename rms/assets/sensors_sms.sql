bc7f4569efc9c3202dae66a17a1eb0cc888cd06d
NaelAwayes <naelawayes@gmail.com>
2017-12-01 14:32:47 +0100

    [SENSORS] Removed useless column from sql patch

--- rms/assets/sensors_sms.sql
+++ rms/assets/sensors_sms.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `sensors` ADD `sms_alert` BOOLEAN NOT NULL DEFAULT FALSE ;
-ALTER TABLE `sensors_alarm` ADD `sms_count_day` INT NOT NULL , ADD `lastsms` DATETIME NOT NULL ;
+ALTER TABLE `sensors_alarm` ADD `sms_count_day` INT NOT NULL ;
