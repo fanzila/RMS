48c1edef33e859f0a2bb7c5f2c2fe0f9f8b4343a
fanzila <pierre@doleans.net>
2018-03-27 10:50:48 +0200

    crud fix

--- rms/application/controllers/Crud.php
+++ rms/application/controllers/Crud.php
@@ -34,8 +34,8 @@ class Crud extends CI_Controller {
 		$crud->fields('id_checklist','name','comment','priority','active','order','day_week_num','day_month_num');
         $crud->required_fields('id_checklist','name','priority','active','order','id_bu');
 		$crud->display_as('priority','Priority<br />(1=normal, 2=medium, 3=high)')->display_as('day_week_num','Number of the day-week<br />(0=Sunday, 1=Monday...)')->display_as('day_month_num','Number of the day-month <br />(1,2,3...28)')->display_as('id_checklist','Checklist');
-		$crud->set_relation('id_checklist','checklists','name');
-
+//		$crud->set_relation('id_checklist','checklists','name');
+		
         $output = $crud->render();
 
 		$this->_example_output($output); 
