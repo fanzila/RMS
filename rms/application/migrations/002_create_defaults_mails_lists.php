8ee39787f98db9d9f498297d5e4497efd104d6f8
Samuel DENIS <samuel@denis.tech>
2018-07-09 11:16:56 +0200

    Prevent migration crash

--- rms/application/migrations/002_create_defaults_mails_lists.php
+++ rms/application/migrations/002_create_defaults_mails_lists.php
@@ -62,6 +62,9 @@ class Migration_Create_defaults_mails_lists extends CI_Migration
 
     foreach ($users_groups as $group => $users)
     {
+      if (!array_key_exists($gruop, $groups_list_mapping))
+        continue;
+
       $lists = $groups_list_mapping[$group];
 
       foreach ($users as $user)
