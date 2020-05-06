a70122ce932622a49f32bd498b0a897d1523eaf7
Pierre Doleans <pierre@doleans.net>
2020-05-06 09:06:37 +0200

    videos training feature

---
+++ rms/application/controllers/Videos.php
@@ -0,0 +1,46 @@
+<?php
+class Videos extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library("hmw");
+		$this->load->library('ion_auth');
+		$this->load->library('ion_auth_acl');
+	}
+
+	public function index($login=null)
+	{
+		$this->hmw->keyLogin();
+		$this->hmw->changeBu();// GENERIC changement de Bu
+		
+		$id_bu =  $this->session->userdata('bu_id');
+		$bu_name	= $this->session->userdata('bu_name');
+				
+		$user					= $this->ion_auth->user()->row();
+		$user_groups 			= $this->ion_auth->get_users_groups()->result();
+		$bus_list = $this->hmw->getBus(null, $user->id);
+				
+		$this->db->select('bu_info, training_link')->from('bus')->where('id', $id_bu);
+		$res = $this->db->get() or die($this->mysqli->error);
+		$bu_infos = $res->result();
+		
+		$data = array(
+			'username'	=> $user->username,
+			'user_groups'	=> $user_groups[0],
+			'title'		=> 'Book',
+			'keylogin'	=> $this->session->userdata('keylogin'),
+			'bu_infos'		=> $bu_infos[0]->bu_info,
+			'bu_link'		=> $bu_infos[0]->training_link,
+			'bu_name'	=> $this->session->userdata('bu_name')
+			);
+
+		$headers = $this->hmw->headerVars(1, "/news/index/", "Videos");
+		$this->load->view('jq_header_pre', $headers['header_pre']);
+		$this->load->view('jq_header_post', $headers['header_post']);
+		$this->load->view('videos',$data);
+		$this->load->view('jq_footer');
+	}
+}
+?>
---
+++ rms/application/views/videos.php
@@ -0,0 +1,29 @@
+	</div>
+	<script src="/public/plyr/plyr.polyfilled.js"></script>
+	<link rel="stylesheet" href="/public/plyr/plyr.css" />
+	
+	<div data-role="content" data-theme="a">
+		<h3>Partie 1</h3>
+		
+		<script>
+		    const player = new Plyr('#player');
+		</script>
+		<div style="width:480px;height:270px;background:#000">
+		<div class="plyr">
+			<video src="/public/videos/HankTV_PART1.mp4" id="player" controls controlsList="nodownload" playsinline poster="/public/plyr/cover.png" data-plyr-config='{ "title": "Partie 1" }' />
+		</div>
+		</div>
+		<h3>Partie 2</h3>
+
+		<div style="width:480px;height:270px;background:#000">
+		<div class="plyr">
+			<video src="/public/videos/HankTV_PART2.mp4" id="player" controls controlsList="nodownload" playsinline poster="/public/plyr/cover.png" data-plyr-config='{ "title": "Partie 1" }' />
+		</div>
+		</div>
+		
+		<h3>Partie 3</h3>
+		
+		A venir...
+		
+	</div><!-- /content -->
+</div><!-- /page -->
