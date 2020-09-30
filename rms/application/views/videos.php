d0e8011a0a33ea9ae59c9f70ddbd4af180bcaef8
Pierre Doleans <pierre@doleans.net>
2020-09-30 09:10:49 +0200

    Update videos.php

--- rms/application/views/videos.php
+++ rms/application/views/videos.php
@@ -10,26 +10,41 @@
 		</script>
 		<div style="width:100%;height:100%;background:#000">
 		<div class="plyr">
-			<video src="/public/videos/HankTV_PART1.mp4" id="player" controls controlsList="nodownload" playsinline poster="/public/plyr/cover.png" data-plyr-config='{ "title": "Partie 1" }' />
+			<video src="/public/videos/HankTV_PART1.mp4" id="player1" controls controlsList="nodownload" playsinline poster="/public/plyr/cover.png" data-plyr-config='{ "title": "Partie 1" }' />
 		</div>
 		</div>
 		<h3>Partie 2</h3>
 
 		<div style="width:100%;height:100%;background:#000">
 		<div class="plyr">
-			<video src="/public/videos/HankTV_PART2.mp4" id="player" controls controlsList="nodownload" playsinline poster="/public/plyr/cover.png" data-plyr-config='{ "title": "Partie 1" }' />
+			<video src="/public/videos/HankTV_PART2.mp4" id="player2" controls controlsList="nodownload" playsinline poster="/public/plyr/cover.png" data-plyr-config='{ "title": "Partie 2" }' />
 		</div>
 		</div>
 		
-		<h3>Partie 3</h3>
+		<h3>Partie 3-1</h3>
 		
-		A venir...
+		<div style="width:100%;height:100%;background:#000">
+		<div class="plyr">
+			<video src="/public/videos/HankTV_PART3-1.mp4" id="player3-1" controls controlsList="nodownload" playsinline poster="/public/plyr/cover.png" data-plyr-config='{ "title": "Partie 3-1" }' />
+		</div>
+		</div>
+
+		<h3>Partie 3-2</h3>
 		
+		<div style="width:100%;height:100%;background:#000">
+		<div class="plyr">
+			<video src="/public/videos/HankTV_PART3-2.mp4" id="player3-2" controls controlsList="nodownload" playsinline poster="/public/plyr/cover.png" data-plyr-config='{ "title": "Partie 3-2" }' />
+		</div>
+		</div>
+
 	</div><!-- /content -->
 </div><!-- /page -->
 
 	<script type="text/javascript">
 		$(document).ready(function(){
-		   $('#player').bind('contextmenu',function() { return false; });
+		   $('#player1').bind('contextmenu',function() { return false; });
+		   $('#player2').bind('contextmenu',function() { return false; });
+		   $('#player3-1').bind('contextmenu',function() { return false; });
+		   $('#player3-2').bind('contextmenu',function() { return false; });
 		});
 </script>
